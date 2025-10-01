a6f51ebc10cd0c37ab42ae4c7e0af71d2b8d3417
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-10-01 11:42:37 +0300

    deps: add i18n next and next js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import { i18n } from './next-i18next.config';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  i18n: {
+    ...i18n,
+    localeDetection: false,
+  },
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
 };
 
 export default nextConfig;
