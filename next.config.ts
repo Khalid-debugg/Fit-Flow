0db9a40f804ef416724749ed677266dcab254802
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-10-04 22:52:22 +0300

    feat: add localization setup

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from 'next';
-import { i18n } from './next-i18next.config';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  i18n: {
-    ...i18n,
-    localeDetection: false,
-  },
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
