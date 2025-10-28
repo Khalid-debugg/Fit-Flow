fed36f9bf25d31cbdce568f95b701ec59f880a94
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-10-28 07:24:24 +0300

    feat: add tailwind setup

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
@@ -33,6 +34,6 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react(), tailwindcss()]
   }
 })
