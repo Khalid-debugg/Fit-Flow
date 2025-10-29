03b3eb2f8801db877124916318963e065ab3306f
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-10-29 06:48:38 +0300

    feat: add localization for member's page

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,14 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
+import { useTranslation } from 'react-i18next'
+import { useEffect } from 'react'
 import Layout from './components/Layout/Layout'
+import Dashboard from './pages/Dashboard'
+import Members from './pages/Members'
 
 function App() {
+  const { i18n } = useTranslation()
+
+  useEffect(() => {
+    document.documentElement.dir = i18n.language === 'ar' ? 'rtl' : 'ltr'
+    document.documentElement.lang = i18n.language
+  }, [i18n.language])
+
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/members" element={<div>Members Page</div>} />
+          <Route path="/members" element={<Members />} />
           <Route path="/plans" element={<div>Plans Page</div>} />
           <Route path="/checkin" element={<div>Check-in Page</div>} />
           <Route path="/payments" element={<div>Payments Page</div>} />
