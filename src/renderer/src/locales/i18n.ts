f2ef3a56441fb595379290f2390e81a12eada2f3
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-11-10 17:13:11 +0200

    feat: add membership localization

--- src/renderer/src/locales/i18n.ts
+++ src/renderer/src/locales/i18n.ts
@@ -6,15 +6,16 @@ import plansEn from './en/plans.json'
 import commonAr from './ar/common.json'
 import membersAr from './ar/members.json'
 import plansAr from './ar/plans.json'
-
+import membershipsAr from './ar/memberships.json'
+import membershipsEn from './en/memberships.json'
 i18n.use(initReactI18next).init({
   resources: {
-    en: { common: commonEn, members: membersEn, plans: plansEn },
-    ar: { common: commonAr, members: membersAr, plans: plansAr }
+    en: { common: commonEn, members: membersEn, plans: plansEn, memberships: membershipsEn },
+    ar: { common: commonAr, members: membersAr, plans: plansAr, memberships: membershipsAr }
   },
   lng: 'ar',
   fallbackLng: 'en',
-  ns: ['common', 'members', 'plans'],
+  ns: ['common', 'members', 'plans', 'memberships'],
   defaultNS: 'common',
   interpolation: { escapeValue: false }
 })
